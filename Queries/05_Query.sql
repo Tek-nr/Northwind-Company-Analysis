70b6850baf50c22c80e860b719ce28c1186b56b7
Hilal N. Tek <53139667+Tek-nr@users.noreply.github.com>
2023-07-22 16:27:16 +0300

    Update 05_Query.sql

--- Queries/05_Query.sql
+++ Queries/05_Query.sql
@@ -8,7 +8,7 @@ asked you to provide them a list of products with:
 2. their current unit price (formatted to have only 2 decimals)
 3. their previous unit price (formatted to have only 2 decimals)
 4. their percentage increase as:
-(New Number - Original Number) ÷ Original Number × 100 
+(New Number - Original Number) Ã· Original Number Ã— 100 
 (with the result formatted to an integer, e.g 50 for 50%)
 
 Filtered on the following conditions:
@@ -19,11 +19,11 @@ Filtered on the following conditions:
 
 WITH PRICELIST AS (
 	SELECT 
-			P.ProductName,
-			P.ProductID,
-			P.UnitPrice AS PrevPrice,
-			OD.UnitPrice AS CurrentPrice,
-			OD.OrderID
+		P.ProductName,
+		P.ProductID,
+		P.UnitPrice AS PrevPrice,
+		OD.UnitPrice AS CurrentPrice,
+		OD.OrderID
 	FROM [Order Details] AS OD
 	INNER JOIN Products AS P ON P.ProductID = OD.ProductID
 )
@@ -39,8 +39,8 @@ GROUP BY
 	PrevPrice,
 	CurrentPrice
 HAVING 
-COUNT(DISTINCT OrderID) > 10 AND 
-	ROUND((PrevPrice-CurrentPrice)*100/PrevPrice,2)
+	COUNT(DISTINCT OrderID) > 10 
+	AND ROUND((PrevPrice-CurrentPrice)*100/PrevPrice,2)
 	NOT BETWEEN 20 AND 30
 	
 
