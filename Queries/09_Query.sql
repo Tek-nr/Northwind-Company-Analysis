e53172e5b7d93b52558280c995100a949e878633
Hilal <hntek00@gmail.com>
2023-07-30 02:52:00 +0300

    Update 09_Query.sql

--- Queries/09_Query.sql
+++ Queries/09_Query.sql
@@ -7,10 +7,12 @@ In order to help them they asked you to provide them a list of employees with:
 2. their job title
 3. their total sales amount excluding discount (formatted to have only 2 decimals)
 4. their total number of orders
-5. their average amount per order (formatted to have only 2 decimals)
-6. their total discount amount (formatted to have only 2 decimals)
-7. their total sales amount including discount (formatted to have only 2 decimals)
-8. Their total discount percentage (formatted to have only 2 decimals)
+5. their total number of entries (each row of an order)
+6. their average amount per entry (formatted to have only 2 decimals)
+7. their average amount per order (formatted to have only 2 decimals)
+8. their total discount amount (formatted to have only 2 decimals)
+9. their total sales amount including discount (formatted to have only 2 decimals)
+10. Their total discount percentage (formatted to have only 2 decimals)
 
 Finally order the results by total sales amount including discount (descending).
 ***********************************/
@@ -22,7 +24,8 @@ SELECT
 	ROUND(SUM(OD.UnitPrice*OD.Quantity),2) AS TotalSaleAmountExcludingDiscount,
 	ROUND(SUM((1-OD.Discount)*(OD.UnitPrice*OD.Quantity)),2) AS TotalSaleAmountIncludingDiscount,
 	ROUND(SUM(OD.Discount*OD.UnitPrice*OD.Quantity),2) AS TotalDiscountAmount,
-	COUNT(DISTINCT OD.OrderID) AS TotalOrder
+	COUNT(DISTINCT OD.OrderID) AS TotalOrder,
+	COUNT(*) AS TotalEntries
 FROM	
 	Employees E
 INNER JOIN
@@ -30,7 +33,6 @@ INNER JOIN
 INNER JOIN 
 	[Order Details] OD ON OD.OrderID = O.OrderID
 GROUP BY
-	E.EmployeeID,
 	CONCAT(E.FirstName,' ', E.LastName),
 	E.Title
 )
@@ -38,6 +40,7 @@ GROUP BY
 SELECT
 	* ,
 	ROUND(SUM(TotalSaleAmountExcludingDiscount/TotalOrder),2) AS AverageAmountPerOrder,
+	ROUND(SUM(TotalSaleAmountExcludingDiscount/TotalEntries),2) AS AverageAmountPerEntry,
 	ROUND(SUM((TotalDiscountAmount*100)/TotalSaleAmountExcludingDiscount),2) AS TotalDiscountPercentage
 FROM 
 	KPI
@@ -47,6 +50,7 @@ GROUP BY
 	TotalSaleAmountExcludingDiscount,
 	TotalSaleAmountIncludingDiscount,
 	TotalDiscountAmount,
-	TotalOrder
+	TotalOrder,
+	TotalEntries
 ORDER BY
 	TotalSaleAmountIncludingDiscount DESC
